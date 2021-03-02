6af3a4afd87030fb953d4d9b018e5ca2fcd548a6
cannonf <70234975+cannonf@users.noreply.github.com>
2021-03-02 14:54:06 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,252 +1,200 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
-  let squares = Array.from(document.querySelectorAll('.grid div'))
-  const scoreDisplay = document.querySelector('#score')
-  const startBtn = document.querySelector('#start-button')
-  const width = 10
-  let nextRandom = 0
-  let timerId
-  let score = 0
-  const colors = [
-    'orange',
-    'red',
-    'purple',
-    'green',
-    'blue'
-  ]
 
-  //The Tetrominoes
-  const lTetromino = [
-    [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2+2],
-    [1, width+1, width*2+1, width*2],
-    [width, width*2, width*2+1, width*2+2]
-  ]
-
-  const zTetromino = [
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1],
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1]
-  ]
-
-  const tTetromino = [
-    [1,width,width+1,width+2],
-    [1,width+1,width+2,width*2+1],
-    [width,width+1,width+2,width*2+1],
-    [1,width,width+1,width*2+1]
-  ]
-
-  const oTetromino = [
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1]
-  ]
-
-  const iTetromino = [
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3],
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3]
-  ]
-
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
-
-  let currentPosition = 4
-  let currentRotation = 0
-
-  console.log(theTetrominoes[0][0])
-
-  //randomly select a Tetromino and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length)
-  let current = theTetrominoes[random][currentRotation]
-
-  //draw the Tetromino
-  function draw() {
-    current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino')
-      squares[currentPosition + index].style.backgroundColor = colors[random]
-    })
-  }
-
-  //undraw the Tetromino
-  function undraw() {
-    current.forEach(index => {
-      squares[currentPosition + index].classList.remove('tetromino')
-      squares[currentPosition + index].style.backgroundColor = ''
-
-    })
-  }
-
-  //assign functions to keyCodes
-  function control(e) {
-    if(e.keyCode === 37) {
-      moveLeft()
-    } else if (e.keyCode === 38) {
-      rotate()
-    } else if (e.keyCode === 39) {
-      moveRight()
-    } else if (e.keyCode === 40) {
-      moveDown()
-    }
-  }
-  document.addEventListener('keyup', control)
-
-  //move down function
-  function moveDown() {
-    undraw()
-    currentPosition += width
-    draw()
-    freeze()
-  }
-
-  //freeze function
-  function freeze() {
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-      //start a new tetromino falling
-      random = nextRandom
-      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
-      current = theTetrominoes[random][currentRotation]
-      currentPosition = 4
-      draw()
-      displayShape()
-      addScore()
-      gameOver()
-    }
-  }
-
-  //move the tetromino left, unless is at the edge or there is a blockage
-  function moveLeft() {
-    undraw()
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
-    if(!isAtLeftEdge) currentPosition -=1
-    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition +=1
-    }
-    draw()
-  }
-
-  //move the tetromino right, unless is at the edge or there is a blockage
-  function moveRight() {
-    undraw()
-    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
-    if(!isAtRightEdge) currentPosition +=1
-    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition -=1
-    }
-    draw()
-  }
-
+    const squares = document.querySelectorAll('.grid div')
+    const logsLeft = document.querySelectorAll('.log-left')
+    const logsRight = document.querySelectorAll('.log-right')
+    const carsLeft = document.querySelectorAll('.car-left')
+    const carsRight = document.querySelectorAll('.car-right')
+    const timeLeft = document.querySelector('#time-left')
+    const result = document.querySelector('#result')
+    const startBtn = document.querySelector('#button')
+    const width = 9
+    let currentIndex = 76
+    let currentTime = 20
+    let timerId
   
-  ///FIX ROTATION OF TETROMINOS A THE EDGE 
-  function isAtRight() {
-    return current.some(index=> (currentPosition + index + 1) % width === 0)  
-  }
-  
-  function isAtLeft() {
-    return current.some(index=> (currentPosition + index) % width === 0)
-  }
-  
-  function checkRotatedPosition(P){
-    P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
-    if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
-      if (isAtRight()){            //use actual position to check if it's flipped over to right side
-        currentPosition += 1    //if so, add one to wrap it back around
-        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
-        }
+    //move the Frog
+    function moveFrog(e) {
+      squares[currentIndex].classList.remove('frog')
+      switch(e.keyCode) {
+        case 37:
+          if(currentIndex % width !== 0) currentIndex -= 1
+          break
+        case 38:
+          if(currentIndex - width >= 0) currentIndex -= width
+          break
+        case 39:
+          if(currentIndex % width < width - 1) currentIndex += 1
+          break
+        case 40:
+          if (currentIndex + width < width * width) currentIndex += width
+          break
+      }
+      squares[currentIndex].classList.add('frog')
+      lose()
+      win()
     }
-    else if (P % width > 5) {
-      if (isAtLeft()){
-        currentPosition -= 1
-      checkRotatedPosition(P)
+  
+    //move the cars
+    function autoMoveCars() {
+      carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+      carsRight.forEach(carRight => moveCarRight(carRight))
+    }
+  
+    //move the car left on a time loop
+    function moveCarLeft(carLeft) {
+      switch (true) {
+        case carLeft.classList.contains('c1'):
+        carLeft.classList.remove('c1')
+        carLeft.classList.add('c2')
+        break
+        case carLeft.classList.contains('c2'):
+        carLeft.classList.remove('c2')
+        carLeft.classList.add('c3')
+        break
+        case carLeft.classList.contains('c3'):
+        carLeft.classList.remove('c3')
+        carLeft.classList.add('c1')
+        break
       }
     }
-  }
   
-  //rotate the tetromino
-  function rotate() {
-    undraw()
-    currentRotation ++
-    if(currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
-      currentRotation = 0
+    //move the car right on a time loop
+    function moveCarRight(carRight) {
+      switch (true) {
+        case carRight.classList.contains('c1'):
+        carRight.classList.remove('c1')
+        carRight.classList.add('c3')
+        break
+        case carRight.classList.contains('c2'):
+        carRight.classList.remove('c2')
+        carRight.classList.add('c1')
+        break
+        case carRight.classList.contains('c3'):
+        carRight.classList.remove('c3')
+        carRight.classList.add('c2')
+        break
+      }
     }
-    current = theTetrominoes[random][currentRotation]
-    checkRotatedPosition()
-    draw()
-  }
-  /////////
-
   
-  
-  //show up-next tetromino in mini-grid display
-  const displaySquares = document.querySelectorAll('.mini-grid div')
-  const displayWidth = 4
-  const displayIndex = 0
-
-
-  //the Tetrominos without rotations
-  const upNextTetrominoes = [
-    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
-    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
-    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
-    [0, 1, displayWidth, displayWidth+1], //oTetromino
-    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
-  ]
-
-  //display the shape in the mini-grid display
-  function displayShape() {
-    //remove any trace of a tetromino form the entire grid
-    displaySquares.forEach(square => {
-      square.classList.remove('tetromino')
-      square.style.backgroundColor = ''
-    })
-    upNextTetrominoes[nextRandom].forEach( index => {
-      displaySquares[displayIndex + index].classList.add('tetromino')
-      displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
-    })
-  }
-
-  //add functionality to the button
-  startBtn.addEventListener('click', () => {
-    if (timerId) {
-      clearInterval(timerId)
-      timerId = null
-    } else {
-      draw()
-      timerId = setInterval(moveDown, 1000)
-      nextRandom = Math.floor(Math.random()*theTetrominoes.length)
-      displayShape()
+    //move the logs
+    function autoMoveLogs() {
+      logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+      logsRight.forEach(logRight => moveLogRight(logRight))
     }
+  
+    //logs going left
+    function moveLogLeft(logLeft) {
+      switch (true) {
+        case logLeft.classList.contains('l1'):
+        logLeft.classList.remove('l1')
+        logLeft.classList.add('l2')
+        break
+        case logLeft.classList.contains('l2'):
+        logLeft.classList.remove('l2')
+        logLeft.classList.add('l3')
+        break
+        case logLeft.classList.contains('l3'):
+        logLeft.classList.remove('l3')
+        logLeft.classList.add('l4')
+        break
+        case logLeft.classList.contains('l4'):
+        logLeft.classList.remove('l4')
+        logLeft.classList.add('l5')
+        break
+        case logLeft.classList.contains('l5'):
+        logLeft.classList.remove('l5')
+        logLeft.classList.add('l1')
+        break
+      }
+    }
+  
+    //logs going right
+    function moveLogRight(logRight) {
+      switch (true) {
+        case logRight.classList.contains('l1'):
+        logRight.classList.remove('l1')
+        logRight.classList.add('l5')
+        break
+        case logRight.classList.contains('l2'):
+        logRight.classList.remove('l2')
+        logRight.classList.add('l1')
+        break
+        case logRight.classList.contains('l3'):
+        logRight.classList.remove('l3')
+        logRight.classList.add('l2')
+        break
+        case logRight.classList.contains('l4'):
+        logRight.classList.remove('l4')
+        logRight.classList.add('l3')
+        break
+        case logRight.classList.contains('l5'):
+        logRight.classList.remove('l5')
+        logRight.classList.add('l4')
+        break
+      }
+    }
+  
+    //move the frog when its on the log moving left
+    function moveWithLogLeft() {
+      if (currentIndex >= 27 && currentIndex < 35) {
+        squares[currentIndex].classList.remove('frog')
+        currentIndex += 1
+        squares[currentIndex].classList.add('frog')
+      }
+    }
+  
+    //move the frog when its on the log moving right
+    function moveWithLogRight() {
+      if (currentIndex > 18 && currentIndex <= 26) {
+        squares[currentIndex].classList.remove('frog')
+        currentIndex -= 1
+        squares[currentIndex].classList.add('frog')
+      }
+    }
+  
+    //rules for frog to win
+    function win() {
+      if (squares[4].classList.contains('frog')) {
+        result.innerHTML = 'You WON'
+        squares[currentIndex].classList.remove('frog')
+        clearInterval(timerId)
+        document.removeEventListener('keyup', moveFrog)
+      }
+    }
+  
+    //rules for frog to lose
+    function lose() {
+      if ((currentTime === 0 ) || (squares[currentIndex].classList.contains('c1')) 
+      || (squares[currentIndex].classList.contains('l5'))
+      || (squares[currentIndex].classList.contains('l4'))
+      ) {
+        result.innerHTML = 'You LOSE'
+        squares[currentIndex].classList.remove('frog')
+        clearInterval(timerId)
+        document.removeEventListener('keyup', moveFrog)
+      }
+    }
+      
+    //all the functions that move pieces
+    function movePieces() {
+      currentTime--
+      timeLeft.textContent = currentTime
+      autoMoveCars()
+      autoMoveLogs()
+      moveWithLogLeft()
+      moveWithLogRight()
+      lose()
+    }
+  
+    //to start, and pause the game
+    startBtn.addEventListener('click', () => {
+      if(timerId) {
+        clearInterval(timerId)
+      } else {
+        timerId = setInterval(movePieces, 1000)
+        document.addEventListener('keyup', moveFrog)
+      }
+    })
+  
   })
-
-  //add score
-  function addScore() {
-    for (let i = 0; i < 199; i +=width) {
-      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
-
-      if(row.every(index => squares[index].classList.contains('taken'))) {
-        score +=10
-        scoreDisplay.innerHTML = score
-        row.forEach(index => {
-          squares[index].classList.remove('taken')
-          squares[index].classList.remove('tetromino')
-          squares[index].style.backgroundColor = ''
-        })
-        const squaresRemoved = squares.splice(i, width)
-        squares = squaresRemoved.concat(squares)
-        squares.forEach(cell => grid.appendChild(cell))
-      }
-    }
-  }
-
-  //game over
-  function gameOver() {
-    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = 'end'
-      clearInterval(timerId)
-    }
-  }
-
-})
