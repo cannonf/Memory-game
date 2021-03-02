3bf39fd6d1bd9ed5def4964a69340ee7b38f447b
cannonf <70234975+cannonf@users.noreply.github.com>
2021-03-02 14:59:28 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,118 +1,94 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //card options
-
- 
-    const cardArray = [
-      {
-        name: 'fries',
-        img: 'images/fries.png'
-      },
-      {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
-      },
-      {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
-      },
-      {
-        name: 'pizza',
-        img: 'images/pizza.png'
-      },
-      {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
-      },
-      {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
-      },
-      {
-        name: 'fries',
-        img: 'images/fries.png'
-      },
-      {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
-      },
-      {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
-      },
-      {
-        name: 'pizza',
-        img: 'images/pizza.png'
-      },
-      {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
-      },
-      {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('.start')
+  
+    const width = 10
+    let currentIndex = 0 //so first div in our grid
+    let appleIndex = 0 //so first div in our grid
+    let currentSnake = [2,1,0] 
+    let direction = 1
+    let score = 0
+    let speed = 0.9
+    let intervalTime = 0
+    let interval = 0
+  
+  
+    //to start, and restart the game
+    function startGame() {
+      currentSnake.forEach(index => squares[index].classList.remove('snake'))
+      squares[appleIndex].classList.remove('apple')
+      clearInterval(interval)
+      score = 0
+      randomApple()
+      direction = 1
+      scoreDisplay.innerText = score
+      intervalTime = 1000
+      currentSnake = [2,1,0]
+      currentIndex = 0
+      currentSnake.forEach(index => squares[index].classList.add('snake'))
+      interval = setInterval(moveOutcomes, intervalTime)
+    }
+  
+  
+    //function that deals with ALL the ove outcomes of the Snake
+    function moveOutcomes() {
+  
+      //deals with snake hitting border and snake hitting self
+      if (
+        (currentSnake[0] + width >= (width * width) && direction === width ) || //if snake hits bottom
+        (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
+        (currentSnake[0] - width < 0 && direction === -width) ||  //if snake hits the top
+        squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
+      ) {
+        return clearInterval(interval) //this will clear the interval if any of the above happen
       }
-    ]
   
-    cardArray.sort(() => 0.5 - Math.random())
+      const tail = currentSnake.pop() //removes last ite of the array and shows it
+      squares[tail].classList.remove('snake')  //removes class of snake from the TAIL
+      currentSnake.unshift(currentSnake[0] + direction) //gives direction to the head of the array
   
-    const grid = document.querySelector('.grid')
-    const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
+      //deals with snake getting apple
+      if(squares[currentSnake[0]].classList.contains('apple')) {
+        squares[currentSnake[0]].classList.remove('apple')
+        squares[tail].classList.add('snake')
+        currentSnake.push(tail)
+        randomApple()
+        score++
+        scoreDisplay.textContent = score
+        clearInterval(interval)
+        intervalTime = intervalTime * speed
+        interval = setInterval(moveOutcomes, intervalTime)
+      }
+      squares[currentSnake[0]].classList.add('snake')
+    }
   
-    //create your board
-    function createBoard() {
-      for (let i = 0; i < cardArray.length; i++) {
-        const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
-        card.setAttribute('data-id', i)
-        card.addEventListener('click', flipCard)
-        grid.appendChild(card)
+  
+    //generate new apple once apple is eaten
+    function randomApple() {
+      do{
+        appleIndex = Math.floor(Math.random() * squares.length)
+      } while(squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
+      squares[appleIndex].classList.add('apple')
+    }
+  
+  
+    //assign functions to keycodes
+    function control(e) {
+      squares[currentIndex].classList.remove('snake')
+  
+      if(e.keyCode === 39) {
+        direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
+      } else if (e.keyCode === 38) {
+        direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
+      } else if (e.keyCode === 37) {
+        direction = -1 // if we press left, the snake will go left one div
+      } else if (e.keyCode === 40) {
+        direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
       }
     }
   
-    //check for matches
-    function checkForMatch() {
-      const cards = document.querySelectorAll('img')
-      const optionOneId = cardsChosenId[0]
-      const optionTwoId = cardsChosenId[1]
-      
-      if(optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', 'images/blank.png')
-        cards[optionTwoId].setAttribute('src', 'images/blank.png')
-        alert('You have clicked the same image!')
-      }
-      else if (cardsChosen[0] === cardsChosen[1]) {
-        alert('You found a match')
-        cards[optionOneId].setAttribute('src', 'images/white.png')
-        cards[optionTwoId].setAttribute('src', 'images/white.png')
-        cards[optionOneId].removeEventListener('click', flipCard)
-        cards[optionTwoId].removeEventListener('click', flipCard)
-        cardsWon.push(cardsChosen)
-      } else {
-        cards[optionOneId].setAttribute('src', 'images/blank.png')
-        cards[optionTwoId].setAttribute('src', 'images/blank.png')
-        alert('Sorry, try again')
-      }
-      cardsChosen = []
-      cardsChosenId = []
-      resultDisplay.textContent = cardsWon.length
-      if  (cardsWon.length === cardArray.length/2) {
-        resultDisplay.textContent = 'Congratulations! You found them all!'
-      }
-    }
-  
-    //flip your card
-    function flipCard() {
-      let cardId = this.getAttribute('data-id')
-      cardsChosen.push(cardArray[cardId].name)
-      cardsChosenId.push(cardId)
-      this.setAttribute('src', cardArray[cardId].img)
-      if (cardsChosen.length ===2) {
-        setTimeout(checkForMatch, 500)
-      }
-    }
-  
-    createBoard()
+    document.addEventListener('keyup', control)
+    startBtn.addEventListener('click', startGame)
   })
